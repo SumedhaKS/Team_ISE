89f481c9fe2b0327e936cc8ccfa460e3358abf5f
SumedhaKS <kssumedha24@gmail.com>
2024-05-13 21:45:14 +0530

    just integrated front & back end with django (no functionalities added)

--- project_AI/project_AI/settings.py
+++ project_AI/project_AI/settings.py
@@ -31,13 +31,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'myapp.apps.MyappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'myapp',
 ]
 
 MIDDLEWARE = [
--- project_AI/project_AI/urls.py
+++ project_AI/project_AI/urls.py
@@ -15,10 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from . import views
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.test, name='test'),
+    path('', include('myapp.urls')),
 ]
