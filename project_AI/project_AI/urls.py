a2741d371ce16848620b8e614fb72c05d5df49a5
Sks <abc@gmail.com>
2024-05-13 21:33:28 +0530

    Revert "just integrated front & back end with django (no functionalities added))"
    This reverts commit 6913fc9796ab2c59cbecc25556b0a35ea2a5dfd4.

--- project_AI/project_AI/urls.py
+++ project_AI/project_AI/urls.py
@@ -15,11 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
-from myapp import views
-from django.conf import settings
+from django.urls import path
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('myapp.urls')),
+    path('', views.test, name='test'),
 ]
